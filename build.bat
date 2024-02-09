483d63f694d853848c36a330ab97dc39e5b040bc
Shaw <shawwian@gmail.com>
2024-02-08 20:18:56 -0500

    decompress with zlib, untar with microtar

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
 setlocal
 set "src_dir=%~dp0"
+set cflags=/Zi /nologo /W4 /MD /D_CRT_SECURE_NO_WARNINGS /I%src_dir% /Fetarball_extract.exe
 if not exist build\ mkdir build
 pushd build
-cl /Zi /nologo /W4 /MD /D_CRT_SECURE_NO_WARNINGS /I%src_dir% %src_dir%main.c %src_dir%microtar.c %src_dir%zlib.lib
+cl %cflags% %src_dir%main.c %src_dir%microtar.c %src_dir%zlib.lib kernel32.lib
 popd
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <assert.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -8,153 +9,159 @@
 
 #define CHUNK_SIZE 262144
 
-static int gzip_write(mtar_t *tar, const void *data, unsigned size) {
-	(void)tar; (void)data; (void)size;
-	// write should not get called for uncompressing tarballs
-	assert(0 && "not implemented");
-	return 0;
+static int default_file_write(mtar_t *tar, const void *data, unsigned size) {
+  size_t res = fwrite(data, 1, size, tar->stream);
+  return (res == size) ? MTAR_ESUCCESS : MTAR_EWRITEFAIL;
 }
 
-static int gzip_read(mtar_t *tar, void *data, unsigned size) {
-	unsigned res = gzread(tar->stream, data, size);
-	return (res == size) ? MTAR_ESUCCESS : MTAR_EREADFAIL;
+static int default_file_read(mtar_t *tar, void *data, unsigned size) {
+  size_t res = fread(data, 1, size, tar->stream);
+  return (res == size) ? MTAR_ESUCCESS : MTAR_EREADFAIL;
 }
 
-static int gzip_seek(mtar_t *tar, unsigned offset) {
-	// NOTE(shaw): zlib docs say that if the file is opened for reading, this
-	// function is emulated but can be extremely slow 
-	// see https://www.zlib.net/manual.html#Gzip
-	z_off_t res = gzseek(tar->stream, offset, SEEK_SET);
-	return (res == -1) ? MTAR_ESEEKFAIL: MTAR_ESUCCESS;
+static int default_file_seek(mtar_t *tar, unsigned offset) {
+  int res = fseek(tar->stream, offset, SEEK_SET);
+  return (res == 0) ? MTAR_ESUCCESS : MTAR_ESEEKFAIL;
 }
 
-static int gzip_close(mtar_t *tar) {
-  int res = gzclose(tar->stream);
-  return res == Z_OK ? MTAR_ESUCCESS : MTAR_EFAILURE;
+static int default_file_close(mtar_t *tar) {
+  fclose(tar->stream);
+  return MTAR_ESUCCESS;
 }
 
-// int gzip_decompress(gzFile in_file, FILE *out_file) {
-	// char out[CHUNK_SIZE];
-	// int uncompressed_bytes = 0;
-	// size_t bytes_written = 0;
-	// int result = Z_OK;
-
-	// for (;;) {
-		// uncompressed_bytes = gzread(in_file, out, CHUNK_SIZE);
-		// if (uncompressed_bytes == -1) {
-			// result = Z_STREAM_ERROR;
-			// break;
-		// } 
-
-		// bytes_written = fwrite(out, 1, uncompressed_bytes, out_file);
-		// if (bytes_written != uncompressed_bytes || ferror(out_file)) {
-			// result = Z_ERRNO;
-			// break;
-		// }
-
-		// if (uncompressed_bytes < CHUNK_SIZE) {
-			// break;
-		// }
-	// } 
-	// return result;
-// }
-
-int tarball_extract(char *in_path, char *out_path) {
-	(void)out_path;
-	// char *p;
-	mtar_header_t h;
-	mtar_t tar = {0};
-	tar.write = gzip_write;
-	tar.read = gzip_read;
-	tar.seek = gzip_seek;
-	tar.close = gzip_close;
-
-	tar.stream = gzopen(in_path, "rb");
-	if (!tar.stream) {
+static int write_entire_file(char *path, char *buf, size_t size) {
+	FILE *file = fopen(path, "wb");
+	if (!file) {
 		return MTAR_EOPENFAIL;
 	}
+	size_t bytes_written = fwrite(buf, 1, size, file);
+	fclose(file);
+	return (bytes_written == size) ? MTAR_ESUCCESS : MTAR_EWRITEFAIL;
+}
 
-	/* Read first header to check it is valid if mode is `r`
-	   if (*mode == 'r') {
-	   err = mtar_read_header(tar, &h);
-	   if (err != MTAR_ESUCCESS) {
-	   mtar_close(tar);
-	   return err;
-	   }
-	   }
-	   */
+static int create_directory(char *path) {
+	BOOL ok = CreateDirectoryA(path, NULL);
+	return ok ? MTAR_ESUCCESS : MTAR_EWRITEFAIL;
+}
+
+int gzip_decompress(gzFile in_file, FILE *out_file) {
+	char out[CHUNK_SIZE];
+	int uncompressed_bytes = 0;
+	size_t bytes_written = 0;
+	int result = Z_OK;
+
+	for (;;) {
+		uncompressed_bytes = gzread(in_file, out, CHUNK_SIZE);
+		if (uncompressed_bytes == -1) {
+			result = Z_STREAM_ERROR;
+			break;
+		} 
+
+		bytes_written = fwrite(out, 1, uncompressed_bytes, out_file);
+		if (bytes_written != uncompressed_bytes || ferror(out_file)) {
+			result = Z_ERRNO;
+			break;
+		}
+
+		if (uncompressed_bytes < CHUNK_SIZE) {
+			break;
+		}
+	} 
+	return result;
+}
+
+int untar(FILE *file) {
+	if (fseek(file, 0, SEEK_SET) != 0) {
+		return MTAR_ESEEKFAIL;
+	}
+
+	mtar_header_t h;
+	mtar_t tar = {0};
+	tar.write = default_file_write;
+	tar.read = default_file_read;
+	tar.seek = default_file_seek;
+	tar.close = default_file_close;
+	tar.stream = file;
 
 	int err = mtar_read_header(&tar, &h);
-	while (err == MTAR_ESUCCESS) {
+	while (err == MTAR_ESUCCESS) { 
 		printf("%s (%d bytes)\n", h.name, h.size);
+
+		if (h.type == MTAR_TDIR) {
+			create_directory(h.name);
+		} else if (h.type == MTAR_TREG) {
+			char *buf = calloc(1, h.size + 1);
+			mtar_read_data(&tar, buf, h.size);
+			err = write_entire_file(h.name, buf, h.size);
+			free(buf);
+			if (err != MTAR_ESUCCESS) {
+				break;
+			}
+		} else {
+			err = MTAR_EREADFAIL;
+			break;
+		}
+
 		mtar_next(&tar);
 		err = mtar_read_header(&tar, &h);
 	}
-
-	if (err == MTAR_ENULLRECORD) {
-		err = MTAR_ESUCCESS;
-	}
-
-	return err;
+	
+	return 0;
 }
 
 int main(int argc, char **argv) {
-	if (argc < 3) {
-		printf("Usage: %s <INPUT FILEPATH> <OUTPUT FILEPATH>\n", argv[0]);
+	if (argc < 2) {
+		printf("Usage: %s FILEPATH\n", argv[0]);
 		return 1;
 	}
 
+	int result = 0;
 	char *in_path = argv[1];
-	char *out_path = argv[2];
+	char *out_path = "_temp_tarfile_";
+	FILE *out_file = NULL;
 
-	int result = tarball_extract(in_path, out_path);
-
-	if (result != MTAR_ESUCCESS) {
-		fprintf(stderr, "error: tarball_extract failed: %s\n", mtar_strerror(result));
-		return 1;
+	gzFile in_file = gzopen(in_path, "rb");
+	if (!in_file) {
+		fprintf(stderr, "Failed to open file: %s\n", in_path);
+		result = 1;
+		goto cleanup;
 	}
 
+	if (gzbuffer(in_file, CHUNK_SIZE) == -1) {
+		fprintf(stderr, "Failed to grow gzbuffer size to: %d\n", CHUNK_SIZE);
+		result = 1;
+		goto cleanup;
+	}
+
+	out_file = fopen(out_path, "wb+");
+	if (!out_file) {
+		fprintf(stderr, "Failed to open file: %s\n", out_path);
+		result = 1;
+		goto cleanup;
+	}
+
+	int err = gzip_decompress(in_file, out_file);
+	if (err != Z_OK) {
+		fprintf(stderr, "gzip_decompress: %s\n", gzerror(in_file, &err));
+		result = 1;
+		goto cleanup;
+	}
+
+	err = untar(out_file);
+	if (err != MTAR_ESUCCESS) {
+		fprintf(stderr, "untar: %s\n", mtar_strerror(err));
+		result = 1;
+	}
+
+cleanup:
+	if (in_file) {
+		gzclose(in_file);
+	}
+	if (out_file) {
+		fclose(out_file);
+		remove(out_path);
+	}
 	return 0;
-
-	// gzFile in_file = gzopen(in_path, "rb");
-	// if (!in_file) {
-		// fprintf(stderr, "Failed to open file: %s\n", in_path);
-		// return 1;
-	// }
-
-	// if (gzbuffer(in_file, CHUNK_SIZE) == -1) {
-		// fprintf(stderr, "Failed to grow gzbuffer size to: %d\n", CHUNK_SIZE);
-		// gzclose(in_file);
-		// return 1;
-	// }
-
-	// FILE *out_file = fopen(out_path, "wb");
-	// if (!out_file) {
-		// fprintf(stderr, "Failed to open file: %s\n", out_path);
-		// gzclose(in_file);
-		// return 1;
-	// }
-
-
-	// int result = gzip_decompress(in_file, out_file);
-
-	// gzclose(in_file);
-	// fclose(out_file);
-
-	// switch (result) {
-		// case Z_OK:
-			// return 0;
-		// case Z_STREAM_ERROR:
-			// fprintf(stderr, "gzip_decompress: Z_STREAM_ERROR\n");
-			// return 1;
-		// case Z_ERRNO:
-			// fprintf(stderr, "gzip_decompress: %s\n", strerror(errno));
-			// return 1;
-		// default:
-			// fprintf(stderr, "gzip_decompress: unknown return code%d\n", result);
-			// return 1;
-	// }
 }
-
 
 
